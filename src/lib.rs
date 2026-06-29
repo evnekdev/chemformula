dcfea315c66211f0ccf87b6acae4e472150d102d
evnekdev <evgnekhoroshev@gmail.com>
2026-06-29 12:41:17 +1000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 //lib.rs
 
 //! This crate helps to parse simple chemical formulas. Its original purpose was to facilitate composition calculations for thermochemical calculations using ChemApp, but it might be useful for general purposes as well. The parser is built using [`nom`] crate functionality. For now, the parsing functionality is limited to parsing simple formulas without parentheses and with float coefficients for elements.
+//! 
+//! Main functionality and structures:
+//! `Element` structure represents a chemical element from the Periodic table + two pseudoelements for chemical thermodynamics (charge compensation electrons and site vacancies).
+//! `Formula` a parsed formula instance: elements + coefficients + formula charge; implement basis arithmetic operations, you can add formulas together and multiply by a coefficient.
+//! `Transform` - a structure to transform between two formula basis sets, for example, from ["CaO", "Al2O3", "SiO2"] to ["Ca", "Al", "Si", "O"]
+
 
 pub mod element;
 pub mod formula;
